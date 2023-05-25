2b347a4c582c7e0ad11590c2d6c72deb12cdc0aa
Peter Okorafor <peterdtitan@gmail.com>
2023-05-25 13:30:34 +0300

    Make changes required

--- student.rb
+++ student.rb
@@ -1,8 +1,6 @@
 require_relative 'person'
 
 class Student < Person
-  attr_reader :classroom, :parent_permission
-
   def initialize(age, classroom, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
