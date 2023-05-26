a59b04506b6aa5206a564d3762c46a34af3351ea
Peter Okorafor <peterdtitan@gmail.com>
2023-05-26 12:34:55 +0300

    Fix person.rb

--- person.rb
+++ person.rb
@@ -1,12 +1,21 @@
-class Person
+require_relative 'nameable'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age, :parent_permission
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
