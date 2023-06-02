89fbbd44e14254076ea24baae2b7a252fd8a503a
Peter Okorafor <peterdtitan@gmail.com>
2023-06-02 18:20:24 +0300

    Preserve data

--- book.rb
+++ book.rb
@@ -7,6 +7,14 @@ class Book
     @rentals = []
   end
 
+  def to_h
+    {
+      title: @title,
+      author: @author,
+      rentals: @rentals.map(&:to_h)
+    }
+  end
+
   def add_rental(person, date)
     Rental.new(date, self, person)
   end
